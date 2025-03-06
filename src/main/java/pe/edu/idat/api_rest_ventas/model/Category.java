9b41eb77187533a0ed3bb3c06471735ff9f4f2f4
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-06 08:27:50 -0500

    Entidades Category y Product con encapsulamiento

--- src/main/java/pe/edu/idat/api_rest_ventas/model/Category.java
+++ src/main/java/pe/edu/idat/api_rest_ventas/model/Category.java
@@ -10,4 +10,28 @@ public class Category {
     private Integer categoryid;
     private String categoryname;
     private String description;
+
+    public Integer getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(Integer categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public String getCategoryname() {
+        return categoryname;
+    }
+
+    public void setCategoryname(String categoryname) {
+        this.categoryname = categoryname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/pe/edu/idat/api_rest_ventas/model/Product.java
+++ src/main/java/pe/edu/idat/api_rest_ventas/model/Product.java
@@ -1,4 +1,94 @@
 package pe.edu.idat.api_rest_ventas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    private Integer productid;
+    private String productname;
+    private String quantityperunit;
+    private Double unitprice;
+    private Integer unitsinstock;
+    private Integer unitsonorder;
+    private Integer reorderlevel;
+    private Boolean discontinued;
+    @ManyToOne
+    @JoinColumn(name = "categoryid")
+    private Category category;
+
+    public Integer getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Integer productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getQuantityperunit() {
+        return quantityperunit;
+    }
+
+    public void setQuantityperunit(String quantityperunit) {
+        this.quantityperunit = quantityperunit;
+    }
+
+    public Double getUnitprice() {
+        return unitprice;
+    }
+
+    public void setUnitprice(Double unitprice) {
+        this.unitprice = unitprice;
+    }
+
+    public Integer getUnitsinstock() {
+        return unitsinstock;
+    }
+
+    public void setUnitsinstock(Integer unitsinstock) {
+        this.unitsinstock = unitsinstock;
+    }
+
+    public Integer getUnitsonorder() {
+        return unitsonorder;
+    }
+
+    public void setUnitsonorder(Integer unitsonorder) {
+        this.unitsonorder = unitsonorder;
+    }
+
+    public Integer getReorderlevel() {
+        return reorderlevel;
+    }
+
+    public void setReorderlevel(Integer reorderlevel) {
+        this.reorderlevel = reorderlevel;
+    }
+
+    public Boolean getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(Boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
