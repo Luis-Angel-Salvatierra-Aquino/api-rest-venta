145135e10978427cc4276acc431e7589983ddd18
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-03-20 08:14:02 -0500

    busqueda parcial con error

--- src/main/java/pe/edu/idat/api_rest_ventas/controller/ProductController.java
+++ src/main/java/pe/edu/idat/api_rest_ventas/controller/ProductController.java
@@ -7,6 +7,7 @@ import pe.edu.idat.api_rest_ventas.dto.DtoEntity;
 import pe.edu.idat.api_rest_ventas.dto.GenericResponseDto;
 import pe.edu.idat.api_rest_ventas.dto.ProductDto;
 import pe.edu.idat.api_rest_ventas.dto.UpdateProductDto;
+import pe.edu.idat.api_rest_ventas.exception.ResourceNotFoundException;
 import pe.edu.idat.api_rest_ventas.model.Category;
 import pe.edu.idat.api_rest_ventas.model.Product;
 import pe.edu.idat.api_rest_ventas.service.ProductService;
@@ -43,7 +44,24 @@ public class ProductController {
         actualizarProducto(@PathVariable Integer id,
                            @RequestBody UpdateProductDto
                                    productDto){
-        Product currentProduct =
-        return null;
+        GenericResponseDto<String> response =
+                new GenericResponseDto<>();
+        try {
+            productService
+                    .obtenerProductoXid(id)
+                    .orElseThrow(() ->
+                            new ResourceNotFoundException("El producto no existe"));
+            productDto.setId(id);
+            productService.actualizarProducto(productDto);
+            response.setRespuesta("Producto actualizado correctamente");
+            response.setStatusCode(200);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.setRespuesta("Error al actualizar producto");
+            response.setStatusCode(500);
+            return new ResponseEntity<>(response,
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
