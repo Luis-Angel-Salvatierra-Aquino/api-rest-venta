e15df996489253731c2a438654b45d2d2aa939c7
Luis-Angel-Salvatierra-Aquino <d18159.github@gmail.com>
2025-04-03 09:00:09 -0500

    repository terminado

--- src/main/java/pe/edu/idat/api_rest_ventas/repository/ConsultasOrderRepository.java
+++ src/main/java/pe/edu/idat/api_rest_ventas/repository/ConsultasOrderRepository.java
@@ -1,12 +1,22 @@
 package pe.edu.idat.api_rest_ventas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.idat.api_rest_ventas.model.GeneralEntity;
+import pe.edu.idat.api_rest_ventas.repository.projection.OrderDatesProjection;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ConsultasOrderRepository
         extends JpaRepository<GeneralEntity, Long> {
 
-    
+    @Query(value = "CALL ObtenerOrdenesPorFecha(:fechaInicio, :fechaFin)",
+    nativeQuery = true)
+    List<OrderDatesProjection> obtenerOrdenesXFecha(
+            @Param("fechaInicio")Date fechaInicio,
+            @Param("fechaFin") Date fechaFin);
 
 
 }
